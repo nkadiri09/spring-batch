753b06645e5c7c8a1b4bfc9fe5e4c4615a64395e
“nkadiri09@gmail.com” <nkadiri09@gmail.com>
2022-12-26 21:53:06 -0500

    Added steps

--- src/main/java/com/techgeeknext/springbootbatch/config/SpringBootBatchConfig.java
+++ src/main/java/com/techgeeknext/springbootbatch/config/SpringBootBatchConfig.java
@@ -1,51 +1,88 @@
 package com.techgeeknext.springbootbatch.config;
 
-import com.techgeeknext.springbootbatch.step.MessageProcessor;
-import com.techgeeknext.springbootbatch.step.MessageReader;
-import com.techgeeknext.springbootbatch.step.MessageWriter;
+import com.techgeeknext.springbootbatch.listner.FirstJobListener;
+import com.techgeeknext.springbootbatch.listner.FirstStepListener;
+import com.techgeeknext.springbootbatch.service.SecondTasklet;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
- 
+
 @Configuration
 public class SpringBootBatchConfig {
- 
-    @Autowired
-    public JobBuilderFactory jobBuilderFactory;
- 
-    @Autowired
-    public StepBuilderFactory stepBuilderFactory;
 
-    /**
-     * A Job is made up of many steps and each step is
-     *  a READ-PROCESS-WRITE task or a single operation task (tasklet).
-      * @return job
-     */
-    @Bean
-    public Job simpleJob() {
-        return jobBuilderFactory.get("simpleJob")
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    private SecondTasklet secondTasklet;
+
+    @Autowired
+    private FirstJobListener firstJobListener;
+
+    @Autowired
+    private FirstStepListener firstStepListener;
+
+    //@Bean
+    public Job firstJob() {
+        return jobBuilderFactory.get("First Job")
                 .incrementer(new RunIdIncrementer())
-                .flow(step1())
-                .end()
+                .start(firstStep())
+                .next(secondStep())
+                .listener(firstJobListener)
                 .build();
     }
 
-    /**
-     * Step consist of an ItemReader, ItemProcessor and an ItemWriter.
-     * @return step
+    private Step firstStep() {
+        return stepBuilderFactory.get("First Step")
+                .tasklet(firstTask())
+                .listener(firstStepListener)
+                .build();
+    }
+
+    private Tasklet firstTask() {
+        return new Tasklet() {
+
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("This is first tasklet step");
+                System.out.println("SEC = " + chunkContext.getStepContext().getStepExecutionContext());
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+
+    private Step secondStep() {
+        return stepBuilderFactory.get("Second Step")
+                .tasklet(secondTasklet)
+                .build();
+    }
+
+    /*
+     * private Tasklet secondTask() { return new Tasklet() {
+     *
+     * @Override public RepeatStatus execute(StepContribution contribution,
+     * ChunkContext chunkContext) throws Exception {
+     * System.out.println("This is second tasklet step"); return
+     * RepeatStatus.FINISHED; } }; }
      */
+
     @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1")
-                .<String, String> chunk(1)
-                .reader(new MessageReader())
-                .processor(new MessageProcessor())
-                .writer(new MessageWriter())
+    public Job secondJob() {
+        return jobBuilderFactory.get("Second Job")
+                .incrementer(new RunIdIncrementer())
+                .start(firstStep())
                 .build();
     }
 
